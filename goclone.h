a7c1bb6abb0cbca191bc671c905d3a1c9d5232e0
Max107 <max@studio107.ru>
2014-12-04 03:23:42 +0600

    Add pid_namespace

--- goclone.h
+++ goclone.h
@@ -22,50 +22,51 @@
 // a call to memmap. This ensures that malloc/free is not used, and that the
 // memory space is clean.
 typedef struct goclone_cmd {
-	// Exec settings.
-	char *path;
-	char **args;
-	char **env;
-	char *dir;
-	char *chroot_dir;
+    // Exec settings.
+    char *path;
+    char **args;
+    char **env;
+    char *dir;
+    char *chroot_dir;
 
-	// File descriptors.
-	int *files;
-	int files_len;
+    // File descriptors.
+    int *files;
+    int files_len;
 
-	// Credentials
-	bool set_credentials;
-	uid_t uid;
-	gid_t gid;
-	gid_t *groups;
-	int groups_len;
+    // Credentials
+    bool set_credentials;
+    uid_t uid;
+    gid_t gid;
+    gid_t *groups;
+    int groups_len;
 
-	// Cgroups tasks files.
-	char **cgroups_tasks_files;
+    // Cgroups tasks files.
+    char **cgroups_tasks_files;
 
-	// Namespaces that should be joined after clone.
-	char *ipc_namespace;
-	char *mount_namespace;
-	char *network_namespace;
-	char *uts_namespace;
-	bool new_ipc_namespace;
-	bool new_network_namespace;
-	bool new_mount_namespace;
-	bool new_pid_namespace;
-	bool new_uts_namespace;
+    // Namespaces that should be joined after clone.
+    char *ipc_namespace;
+    char *mount_namespace;
+    char *network_namespace;
+    char *uts_namespace;
+    char *pid_namespace;
+    bool new_ipc_namespace;
+    bool new_network_namespace;
+    bool new_mount_namespace;
+    bool new_pid_namespace;
+    bool new_uts_namespace;
 
-	// Set to true if the process should double fork.
-	bool double_fork;
+    // Set to true if the process should double fork.
+    bool double_fork;
 
-	// The signal that should be used when the parent dies.
-	int death_signal;
+    // The signal that should be used when the parent dies.
+    int death_signal;
 
-	// The stack that should be used for the clone destination.
-	void *stack;
+    // The stack that should be used for the clone destination.
+    void *stack;
 
-	// If double forking is set then this stack should be set to a block
-	// of memory allocated specifically for it.
-	void *df_stack;
+    // If double forking is set then this stack should be set to a block
+    // of memory allocated specifically for it.
+    void *df_stack;
 
 } goclone_cmd;
 
